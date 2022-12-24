d65f4e730d5011b93db693b21270f1041152a3a2
AbdulRehman2807 <mark2359978@gmail.com>
2022-12-24 16:59:59 +0500

    update

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -36,13 +36,13 @@ export default function Twextarea(props) {
                     <label htmlFor="exampleFormControlTextarea1" className="form-label"><h1>{props.heading}</h1></label>
                     <textarea required className="form-control" value={text} onChange={(e) => { setText(e.target.value) }} id="exampleFormControlTextarea1" rows="8"></textarea>
                 </div>
-                <button style={{ width: "20%" }} onClick={changeToUppercase} className='btn btn-outline-dark text-light bg-dark my-3'>{btnName1} </button>
-                <button style={{ width: "20%" }} onClick={changeToCapetalize} className='btn btn-outline-dark text-light bg-dark my-3 mx-5'>Capatilize </button>
-                <button style={{ width: "20%" }} onClick={clearAllData} className='btn btn-outline-dark text-light bg-dark my-3 '>Clear data </button>
+                <button disabled={text.length === 0} style={{ width: "20%" }} onClick={changeToUppercase} className='btn btn-outline-dark text-light bg-dark my-3'>{btnName1} </button>
+                <button disabled={text.length === 0} style={{ width: "20%" }} onClick={changeToCapetalize} className='btn btn-outline-dark text-light bg-dark my-3 mx-5'>Capatilize </button>
+                <button disabled={text.length === 0} style={{ width: "20%" }} onClick={clearAllData} className='btn btn-outline-dark text-light bg-dark my-3 '>Clear data </button>
             </div>
             <div className='container text-center pt-5'>
-                <h3>here is {text.length} characters and {text.split(" ").length - 1} words.</h3>
-                <h3>{text.split(" ").length - 1} word take's {0.008 * 60 * (text.split(" ").length - 1)} second</h3>
+                <h3>here is {text.length} characters and {text.split(" ").filter((element) => {return element.length !== 0}).length } words.</h3>
+                <h3>{text.split(" ").filter((element) => {return element.length !== 0}).length} word take's {(0.008 * 60 * (text.length)).toFixed(2)} second</h3>
                 <h1>Preview</h1>
                 <p style={{ width: "70%", margin: "0px auto" }} className="my-3 text-right">{text}</p>
             </div>
